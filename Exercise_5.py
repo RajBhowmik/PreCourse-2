7c8a5479fec6753b376fac9dc7a7610ad44bd4f3
RajBhowmik <bhowmick.raj10@gmail.com>
2024-07-20 10:10:23 -0700

    Exercies_5_pre2

--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,14 @@
 # Python program for implementation of Quicksort
-
+'''
+Not good with recursion or quick sort. Need some guidance.
+'''
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
+    pass
   #write your code here
 
 
 def quickSortIterative(arr, l, h):
+    pass
   #write your code here
 
